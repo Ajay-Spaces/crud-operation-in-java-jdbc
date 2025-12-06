0f0c463dbf42f44a39ea8454c36dda4f554947e7
Ajay-Spaces <241801340003@cutmap.ac.in>
2025-12-06 05:49:54 +0000

    Upgrade to Java 21 with Maven and MySQL database integration
    - Add pom.xml with Java 21 target and maven-assembly-plugin for fat JAR
    - Add mysql-connector-j dependency for database connectivity
    - Add .gitignore to exclude build artifacts and IDE files
    - Project now builds and runs with Java 21 LTS
    - Fat JAR includes all dependencies for easy deployment

--- crud/src/Main.java
+++ crud/src/Main.java
@@ -1,16 +1,27 @@
-
 import java.sql.*;
 import java.util.Scanner;
 import static java.lang.System.exit;
 
-
 public class Main {
 
-    public static void createData(){
-        final String url = "jdbc:mysql://localhost:3306/employeedb";
-        final String uname = "root";
-        final String pass = "";
+    // -------------------------------------------
+    // SINGLE CLEAN CONNECTION METHOD (Step 1 Fix)
+    // -------------------------------------------
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");  // IMPORTANT LINE
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/employeedb",
+                "root",
+                ""
+        );
+    }
+
+    // -------------------------------------------
+    // CREATE DATA
+    // -------------------------------------------
+    public static void createData() {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("\t\t\t*Please Remember 'Your ID' number for further Updatation.....");
         System.out.print("\t\t\tEnter employee ID number : ");
         int id = sc.nextInt();
@@ -21,186 +32,171 @@ public class Main {
         System.out.print("\t\t\tEnter employee Location : ");
         String location = sc.next();
 
+        String query = "INSERT INTO employees (id, name, department, location) VALUES ('" + id + "','" + name + "','" + department + "','" + location + "')";
 
-        String query="INSERT INTO employees (id, name, department, location) values ('"+id+"','"+name+"','"+department+"','"+location+"')";
-        Connection con;
-
-        try{
-            con=DriverManager.getConnection(url,uname,pass);
+        try {
+            Connection con = getConnection(); // FIX
             Statement stmt = con.createStatement();
             int rows = stmt.executeUpdate(query);
-            if(rows==1) {
-                System.out.println("\t\t\t rows affected....");
-            }else {
-                System.out.println("\t\t\tSomething is Wrong.....!! ");
-            }
-            menu();
 
+            if (rows == 1) System.out.println("\t\t\tRow inserted successfully!");
+            else System.out.println("\t\t\tSomething is Wrong!");
 
-        }catch (Exception e){
-            e.printStackTrace();
-
-        }
-
-    }
-    public static void readData(){
-        final String url = "jdbc:mysql://localhost:3306/employeedb";
-        final String uname = "root";
-        final String pass = "";
-        String query = "select * from employees";
-
-        Connection con;
-        try {
-            con = DriverManager.getConnection(url, uname, pass);
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            while(rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String department = rs.getString("department");
-                String location = rs.getString("location");
-
-                System.out.println("\t\t\tEmp ID : "+id+"  Name -->> "+name+"    Department -->> "+department+"    Location -->> "+location);
-            }
             menu();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 
-    public static void deleteData(){
-        final String url ="jdbc:mysql://localhost:3306/employeedb";
-        final String uname = "root";
-        final String pass = "";
-        System.out.print("\t\t\tEnter Employee ID number : ");
-        Scanner sc = new Scanner(System.in);
-        int id = sc.nextInt();
-        String query = "DELETE  FROM employees where id ='"+id+"' ";
+    // -------------------------------------------
+    // READ DATA
+    // -------------------------------------------
+    public static void readData() {
+        String query = "SELECT * FROM employees";
 
-        Connection con;
-        try{
-            con = DriverManager.getConnection(url,uname,pass);
-            Statement stmt = con.createStatement();
-            int rows = stmt.executeUpdate(query);
-            if(rows==1) {
-                System.out.println("\t\t\t rows affected....");
-            }else {
-                System.out.println("\t\t\tPlease enter correct ID number.....!! ");
-                deleteData();
+        try {
+            Connection con = getConnection(); // FIX
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String department = rs.getString("department");
+                String location = rs.getString("location");
+
+                System.out.println("\t\t\tEmp ID: " + id + "  Name: " + name + "  Dept: " + department + "  Location: " + location);
             }
+
             menu();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
-    public static void updateData(){
-        final String url ="jdbc:mysql://localhost:3306/employeedb";
-        final String uname = "root";
-        final String pass = "";
-        String query = null;
+    // -------------------------------------------
+    // DELETE DATA
+    // -------------------------------------------
+    public static void deleteData() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("\t\t\t1 - ID no. Updates");
-        System.out.println("\t\t\t2 - Name Updates");
-        System.out.println("\t\t\t3 - Location Updates");
-        System.out.println("\t\t\t4 - Department Updates");
-        System.out.print("\t\t Choose Option to Updates data -->> ");
-        int opt = sc.nextInt();
-        if(opt==1){
-            System.out.print("\n\t\t\tEnter name of Employee : ");
-            String name = sc.next();
-            System.out.print("\t\t\tEnter to updates ID number : ");
-            int id = sc.nextInt();
-            query = "UPDATE employees SET id ='"+id+"' where name ='"+name+"' ";
-        }else if(opt==2){
-            System.out.print("\n\t\t\tEnter employee ID number : ");
-            int id = sc.nextInt();
-            System.out.print("\t\t\tEnter Names to Updates  : ");
-            String name = sc.next();
-            query = "UPDATE employees SET name ='"+name+"' where id ='"+id+"' ";
-        }
-        else if(opt==3){
-            System.out.print("\n\t\t\tEnter employee ID number : ");
-            int id = sc.nextInt();
-            System.out.print("\t\t\tEnter Location to updates  : ");
-            String location = sc.next();
-            query = "UPDATE employees SET location ='"+location+"' where id ='"+id+"' ";
+        System.out.print("\t\t\tEnter Employee ID number : ");
+        int id = sc.nextInt();
+        String query = "DELETE FROM employees WHERE id = '" + id + "'";
 
+        try {
+            Connection con = getConnection(); // FIX
+            Statement stmt = con.createStatement();
+            int rows = stmt.executeUpdate(query);
+
+            if (rows == 1) System.out.println("\t\t\tRow deleted!");
+            else {
+                System.out.println("\t\t\tIncorrect ID. Try again.");
+                deleteData();
+            }
+
+            menu();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else if(opt==4){
-            System.out.print("\n\t\t\tEnter employee ID number : ");
+    }
+
+    // -------------------------------------------
+    // UPDATE DATA
+    // -------------------------------------------
+    public static void updateData() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\t\t\t1 - Update ID");
+        System.out.println("\t\t\t2 - Update Name");
+        System.out.println("\t\t\t3 - Update Location");
+        System.out.println("\t\t\t4 - Update Department");
+        System.out.print("\t\t Choose Option -->> ");
+        int opt = sc.nextInt();
+
+        String query = null;
+
+        if (opt == 1) {
+            System.out.print("\n\t\t\tEnter Name: ");
+            String name = sc.next();
+            System.out.print("\t\t\tEnter New ID: ");
+            int newId = sc.nextInt();
+            query = "UPDATE employees SET id='" + newId + "' WHERE name='" + name + "'";
+        } else if (opt == 2) {
+            System.out.print("\n\t\t\tEnter ID: ");
             int id = sc.nextInt();
-            System.out.print("\t\t\tEnter Department Names to Updates  : ");
-            String department = sc.next();
-            query = "UPDATE employees SET department ='"+department+"' where id ='"+id+"' ";
-        }
-        else {
-            System.out.println("\n\t\t\tChoose correct option ......");
+            System.out.print("\t\t\tEnter New Name: ");
+            String newName = sc.next();
+            query = "UPDATE employees SET name='" + newName + "' WHERE id='" + id + "'";
+        } else if (opt == 3) {
+            System.out.print("\n\t\t\tEnter ID: ");
+            int id = sc.nextInt();
+            System.out.print("\t\t\tEnter New Location: ");
+            String newLoc = sc.next();
+            query = "UPDATE employees SET location='" + newLoc + "' WHERE id='" + id + "'";
+        } else if (opt == 4) {
+            System.out.print("\n\t\t\tEnter ID: ");
+            int id = sc.nextInt();
+            System.out.print("\t\t\tEnter New Department: ");
+            String newDept = sc.next();
+            query = "UPDATE employees SET department='" + newDept + "' WHERE id='" + id + "'";
+        } else {
+            System.out.println("\n\t\t\tInvalid option!");
             updateData();
         }
-        Connection con;
-        try{
-            con = DriverManager.getConnection(url,uname,pass);
+
+        try {
+            Connection con = getConnection(); // FIX
             Statement stmt = con.createStatement();
             int rows = stmt.executeUpdate(query);
-            if(rows==1) {
-                System.out.println("\t\t\t\t\t rows affected....");
-            }else {
-                System.out.println("\t\t\tPlease enter correct data .....!! ");
-                updateData();
-            }
+
+            if (rows == 1) System.out.println("\t\t\tUpdated successfully!");
+            else System.out.println("\t\t\tUpdate failed!");
+
             menu();
-        }catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
+    // -------------------------------------------
+    // MENU
+    // -------------------------------------------
     public static void menu() {
-        System.out.println("\n\n\t\t\t --------- CRUD OPERATION USING JAVA --------\n");
-        System.out.println("\t\t\t1 - Create Data ");
-        System.out.println("\t\t\t2 - Read Data ");
-        System.out.println("\t\t\t3 - Update Data ");
-        System.out.println("\t\t\t4 - Delete Data ");
+        System.out.println("\n\n\t\t\t--------- CRUD OPERATION USING JAVA --------\n");
+        System.out.println("\t\t\t1 - Create Data");
+        System.out.println("\t\t\t2 - Read Data");
+        System.out.println("\t\t\t3 - Update Data");
+        System.out.println("\t\t\t4 - Delete Data");
         System.out.println("\t\t\t5 - Exit");
-        System.out.print("\t\t Select Option  ---->>>  ");
+        System.out.print("\t\t Select Option -->>> ");
+
         Scanner sc = new Scanner(System.in);
-        int select= sc.nextInt();
+        int select = sc.nextInt();
+
         switch (select) {
-            case 1 -> {
-                System.out.println("\n\n\t\t\t ------ Create Employee Data Carefully ------\n");
-                createData();
-            }
-            case 2 -> {
-                System.out.println("\n\n\t\t\t\t\t ---------- Employees Data ---------\n");
-                readData();
-            }
-            case 3 -> {
-                System.out.println("\n\n\t\t\t\t\t --------- Updates Employee Data ---------\n");
-                updateData();
-            }
-            case 4 -> {
-                System.out.println("\n\n\t\t\t\t\t ---------- Delete Employee Data ----------\n");
-                deleteData();
-            }
+            case 1 -> createData();
+            case 2 -> readData();
+            case 3 -> updateData();
+            case 4 -> deleteData();
             case 5 -> {
-                System.out.println("\t\t\t\t\t   Exiting....\n \t\t\t\t\t Thank You...♥");
+                System.out.println("\t\tExit... Thank You ♥");
                 exit(0);
             }
             default -> {
-                System.out.println("Wrong Option...!");
+                System.out.println("Wrong Option!");
                 menu();
             }
         }
-
-
     }
+
+    // -------------------------------------------
+    // MAIN
+    // -------------------------------------------
     public static void main(String[] args) {
         menu();
-
     }
 }
